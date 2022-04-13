b54962ed8753946f2e8253b719953e039943c466
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2022-04-13 20:58:30 +0700

    Add files via upload

---
+++ pipe2.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>     /* exit() */
+#include <unistd.h>     /* fork() and getpid() */
+#include <stdio.h>      /* printf() */
+#include <string.h>
+#include <sys/wait.h>   /* wait */
+#include <errno.h>      /* errno */
+#include <unistd.h>     /* sleep() */
+
+#define MSGSIZE 5
+
+int main(int argc, char **argv) {
+        //char inbuf[MSGSIZE]; //buffer
+        int arrInt[MSGSIZE];
+        int fd[2]; //pipe
+        //create pipe
+        if (pipe(fd) < 0) {
+            exit(1); //error
+        }
+        int produsen, konsumen, i, j;
+        printf("Masukkan jumlah fork produsen\n");
+        scanf("%d", &produsen);
+        printf("Masukkan jumlah fork konsumen\n");
+        scanf("%d", &konsumen);
+
+        int pid;
+        int pidWait, status;
+        printf("mulai \n");
+        switch (pid = fork()) {
+        case 0:         /* fork returns 0 ke proses anak */
+            for(i = 0; i < produsen; i++){
+                fork();
+            }
+
+            // tulis data ke pipe
+            srand(getpid());
+            for(i = 0; i < MSGSIZE; i++){
+                arrInt[i] = rand()%(20+1-0)+0;
+                printf("Produsen %d menulis %d\n", getpid(), arrInt[i]);
+                sleep(1);
+            }
+            write(fd[1], arrInt, sizeof(arrInt));
+            break;
+
+        default:        /* fork returns pid ke proses ortu */
+            for(j = 0; j < konsumen; j++){
+                fork();
+            }
+
+            int jumlah = 0;
+            // baca yang ditulis child dari pipe
+            read(fd[0], arrInt, sizeof(arrInt));
+            for(j = 0; j < MSGSIZE; j++){
+                printf("Konsumen %d mengambil %d\n", getpid(), arrInt[j]);
+                sleep(1);
+            }
+
+            break;
+
+        case -1:        /* error */
+            perror("fork");
+            exit(1);
+        }
+
+        
+        //printf("selesai\n");
+        exit(0);
+}
